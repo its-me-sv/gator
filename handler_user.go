d55e144e894b2b58e52fe2ff4437083ecb70b74b
Suraj Vijay <surajcpp115@gmail.com>
2026-08-16 01:25:21 +0530

    Implement user registration handler in handler_user.go

--- handler_user.go
+++ handler_user.go
@@ -5,7 +5,38 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/its-me-sv/gator/internal/database"
 )
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("missing argument <name>")
+	}
+
+	name := cmd.args[0]
+	user := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+
+	dbUser, err := s.db.CreateUser(context.Background(), user)
+	if err != nil {
+		log.Fatalf("user with name \"%s\" already exists\n", name)
+	}
+	if err := s.cfg.SetUser(name); err != nil {
+		return err
+	}
+
+	fmt.Printf("user \"%s\" was created\n", name)
+	fmt.Printf("%+v\n", dbUser)
+
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
